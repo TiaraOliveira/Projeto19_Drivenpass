427e01729fb4822e01e0117ba8be42099d7d7621
TiaraOliveira <tiaragomes@yahoo.com.br>
2022-09-12 13:44:32 -0300

    fix table

---
+++ prisma/migrations/20220912164239_erase_unique/migration.sql
@@ -0,0 +1,8 @@
+-- DropIndex
+DROP INDEX "card_userId_cardTag_key";
+
+-- DropIndex
+DROP INDEX "credential_userId_credentialTag_key";
+
+-- DropIndex
+DROP INDEX "safeNote_userId_safeNotetitle_key";
